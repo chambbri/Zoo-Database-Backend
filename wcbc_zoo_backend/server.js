8d3801dc7799b60f4370f8ed350c6660ec8347e2
Brian Chamberlain <chambbri@flip1.engr.oregonstate.edu>
2022-03-03 23:16:17 -0800

    connected all pages to database

--- wcbc_zoo_backend/server.js
+++ wcbc_zoo_backend/server.js
@@ -19,9 +19,42 @@ app.get("/", (req, res) => {
   
 });
 
+// Get exhibits
 app.get("/exhibits", (req, res) => {
   const selectExhibits = "SELECT exhibit_id, type, size, animal_capacity FROM Exhibits"
   mysql.pool.query(selectExhibits, (err, result) => {
+    res.send(result);
+  });
+});
+
+//Get animals
+app.get("/animals", (req, res) => {
+  const selectAnimals = "SELECT animal_id, exhibit_id, animal_type, origin_country, birthdate, gender FROM Animals";
+  mysql.pool.query(selectAnimals, (err, result) => {
+    res.send(result);
+  });
+});
+
+//Get employees
+app.get("/employees", (req, res) => {
+  const selectEmployees = "SELECT employee_id, fname, lname, phone, email, job_title FROM Employees";
+  mysql.pool.query(selectEmployees, (err, result) => {
+    res.send(result);
+  });
+});
+
+//Get animal services
+app.get("/animalservices", (req, res) => {
+  const selectAnimalServices = "SELECT animal_services_id, animal_id, date, time, type_of_care FROM Animal_Services";
+  mysql.pool.query(selectAnimalServices, (err, result) => {
+    res.send(result);
+  });
+});
+
+//Get animal employee services
+app.get("/animalemployeeservices", (req, res) => {
+  const selectAnimalEmployeeServices = "SELECT animal_services_id, employee_id FROM Animal_Employee_Services";
+  mysql.pool.query(selectAnimalEmployeeServices, (err, result) => {
     res.send(result);
   });
 });
